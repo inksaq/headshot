3ee4572ad119990c62983fca9de1b62862981d99
inksaq <inksaq@protonmail.com>
2024-01-25 19:14:38 +1300

    headshot client init

---
+++ src/client/main.cpp
@@ -0,0 +1,58 @@
+#include "core/core.h"
+#include "GLFW/glfw3.h"
+#include "thread"
+
+core::graphics::Window window(1600, 900, "Atlas");
+core::Scene* current_scene;
+
+void logic(){
+
+    std::clock_t end = 0;
+
+    while (window.is_open())
+    {
+        std::clock_t start = std::clock();
+        double delta = (double)(start - end) / CLOCKS_PER_SEC;
+        end = std::clock();
+
+        current_scene->update(delta);
+    }
+}
+
+void client(){
+
+    while (window.is_open()){
+        core::Event event;
+
+        glfwPollEvents();
+
+        while (window.poll_event(event)){
+            
+            if (event.type == GLEQ_WINDOW_CLOSED){
+                window.close();
+            }
+
+            if (!window.imgui_want_capture(event)){
+                current_scene->handle_event(event);
+            }
+
+            core::free_event(event);
+        }
+
+        window.new_frame();
+        current_scene->draw(window);
+        window.render();
+    }
+}
+
+int main(){
+    window.create_context();
+
+
+    std::thread logic_thread(logic);
+
+    client();
+
+    logic_thread.join();
+    return 0;
+}
